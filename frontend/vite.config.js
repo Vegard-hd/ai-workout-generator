dd58936fc16ae851988a862f3920694459058e4d
vegard-hd <vegard.hd@outlook.com>
2025-06-26 20:17:58 +0200

    Fixed issue with vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
