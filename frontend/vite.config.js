7052542b2fc7c05c4fbca91ce49d725e19e1caa7
vegard-hd <vegard.hd@outlook.com>
2025-06-26 20:16:25 +0200

    Added build and rollupOptions to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,20 @@ import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
   plugins: [tailwindcss(), preact()],
   server: {
     allowedHosts: [
