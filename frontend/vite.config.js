d64cdc196952a9460b7e0506736792fc88f12497
vegard-hd <vegard.hd@outlook.com>
2025-06-26 21:24:03 +0200

    Cleaned up vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,21 +3,8 @@ import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
   plugins: [tailwindcss(), preact()],
-  base: "/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
   server: {
     allowedHosts: [
       ".my-public-apps-aiworkoutgenerator-lpwjh8-0cee82-51-68-234-12.traefik.me",
